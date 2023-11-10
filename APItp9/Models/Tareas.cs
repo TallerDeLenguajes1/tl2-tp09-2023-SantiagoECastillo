1a732076de4eaf2ab2b89bcaaeb7a4eee4f196c9
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2023-11-10 18:37:03 -0300

    Se crearon los modelos de Usuario, Tarea y Tablero

---
+++ APItp9/Models/Tareas.cs
@@ -0,0 +1,19 @@
+
+public class Tarea{
+    private int idTarea;
+    private string? nombreTarea;
+    private string? descripcionTarea;
+    private string? color;
+    //private EstadoTarea Estado;
+    private int? IdUsuarioAsignado;
+
+}
+
+public enum EstadoTarea{
+    Ideas,
+    ToDo, 
+    Doing, 
+    Review, 
+    Done
+
+}
---
+++ APItp9/Models/Usuarios.cs
@@ -0,0 +1,8 @@
+
+public class Usuario{
+    private int idUsuario;
+    private string? nombreUsuario;
+
+    public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+    public string? NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+}
